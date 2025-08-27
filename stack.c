95f6829accedc66f2020c402f63e2eea67aa4e3f
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-08-27 19:34:13 +0530

    stacks

--- stack.c
+++ stack.c
@@ -5,6 +5,7 @@ int stack[CAPACITY];
 int top=-1;
 void push();
 void Pop();
+void isfull();
 void Display();
 void peek();
 
@@ -20,6 +21,31 @@ void push(){
     scanf("%d",&stack[++top]);
     }
 }
+void peek(){
+    if(top==-1){
+        printf("stack underflow");
+    }
+    else{
+        printf("Toppest element of stack is %d\n",stack[top]);
+    }
+}
+void isfull(){
+    if(top==CAPACITY-1){
+        printf("Stack overflow");
+    }
+    else {
+        printf("Enter the element if stack is not full : %d\n",top-CAPACITY);
+    }
+
+}
+void isempty(){
+    if(top==-1){
+        printf("Stack is empty");
+    }
+    else{
+        printf("Enter the element if stack is not empty: %d\n",CAPACITY-top++);
+    }
+}
 void pop(){
     if(top==-1){
         printf("stack underflow");
@@ -32,13 +58,20 @@ void pop(){
     }
 }
 void Display(){
-    printf("Display stack");
-    printf("enter elements in stack area \n");
+    printf("......Displaying stack..... ");
+    printf("Enter elements in stack area \n");
     for(int i =top;i>=0;i--){
         printf("%d\n",stack[i]);
     }
 }
 int main(){
     push();
+    push();
+    push();
+    push();
+    pop();
+    isempty();
+    isfull();
+    peek();
     Display();
 }
---
+++ stacklocal.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define CAPACITY 5
+void push(int stack[],int *top);
+void Pop(int stack[],int *top);
+void isfull(int top);
+void isempty(int top);
+void Display(int stack[],int top);
+void peek(int stack[],int top);
+
+void push(int stack[],int *top){
+    if (*top==CAPACITY-1){
+        printf("stack overflow");
+    }
+    else{
+        int value;
+        printf("Enter the value you want to enter: ");
+        scanf("%d",&value);
+        stack[++(*top)]=value;
+        printf("%d pushed to stack",value);
+    }
+}
+void peek(int stack[],int top){
+    if(top==-1){
+        printf("stack underflow");
+    }
+    else{
+        printf("Toppest element of stack is %d\n",stack[top]);
+    }
+}
+void isfull(int top){
+    if(top==CAPACITY-1){
+        printf("Stack overflow");
+    }
+    else {
+        printf("Enter the element if stack is not full : %d\n",CAPACITY-top);
+    }
+    
+}
+void isempty(int top){
+    if(top==-1){
+        printf("Stack is empty");
+    }
+    else{
+        printf("Enter the element if stack is not empty: %d\n",CAPACITY-top++);
+    }
+}
+void pop(int stack[],int *top){
+    if(*top==-1){
+        printf("stack underflow");
+    }
+    else{
+        int valuepop;
+        valuepop=stack[*top];
+        printf("enter the value you want to delete: %d\n",valuepop);
+        top--;
+    }
+}
+void Display(int stack[], int top){
+    printf("......Displaying stack..... ");
+    printf("Enter elements in stack area \n");
+    for(int i =top;i>=0;i--){
+        printf("%d\n",stack[i]);
+    }
+}
+int main(){
+    int stack[CAPACITY];
+    int top=-1;
+    push(stack, &top);
+    push(stack,&top);
+    push(stack,&top);
+    push(stack,&top);
+    pop(stack,&top);
+    isempty(top);
+    isfull(top);
+    peek(stack,top);
+    Display(stack,top);
+}
