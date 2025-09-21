81101fced614f063f2d98331b61929ceee48a55c
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-09-21 19:53:08 +0530

    k

--- bubblesort.c
+++ bubblesort.c
@@ -33,7 +33,7 @@ int main()
 
     int arr[n];
 
-    // Taking array element
+    // Taking array element from it
     printf("Enter %d elements:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
