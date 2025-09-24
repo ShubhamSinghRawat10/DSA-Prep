bab1005ad8db560f88f5fec4104db7eb9dee3fef
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-09-24 18:51:00 +0530

    strct

---
+++ struct.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+struct student{
+    int rollno;
+    char name[50];
+    float marks;
+};
+int main()
+{
+    struct student s1;
+    printf("Enter roll number: ");
+    scanf("%d", &s1.rollno);
+    printf("Enter name: ");
+    scanf("%s", s1.name);
+    printf("Enter marks: ");
+    scanf("%f", &s1.marks);
+    struct student s2;
+    printf("Enter roll number: ");
+    scanf("%d", &s2.rollno);
+    printf("Enter name: ");
+    scanf("%s", s2.name);
+    printf("Enter marks: ");
+    scanf("%f", &s2.marks);
+
+    //display ke liye
+    printf("\nStudent Details:\n");
+    printf("Roll Number: %d\n", s1.rollno);
+    printf("Name: %s\n", s1.name);
+    printf("Marks: %.f\n", s1.marks);
+    printf("\n");
+    printf("Roll Number: %d\n", s2.rollno);
+    printf("Name: %s\n", s2.name);
+    printf("Marks: %.f\n", s2.marks);
+    return 0;
+}
