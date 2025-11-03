b74912759b7b293e17702bac1eed030b7052f43e
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-11-04 00:35:59 +0530

    hmm

--- queuelocal.c
+++ queuelocal.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 #define QUEUE_MAX 31
-//tough h padhle bhai plz
+//tough h padhle bhai plz smjh
 void enqueue(int QUEUE[], int *FRONT, int *REAR);
 void dequeue(int QUEUE[], int *FRONT, int *REAR);
 void peek(int QUEUE[], int FRONT, int REAR);
