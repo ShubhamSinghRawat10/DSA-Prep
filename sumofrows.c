c05c204c601e000dc95f20ab911603fcf6cd0038
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-12-05 21:18:29 +0530

    function

--- sumofrows.c
+++ sumofrows.c
@@ -14,7 +14,7 @@ void createArr(int arr[MAX][MAX], int *row, int *col) {
         }
     }
 }
-
+//print
 void printArr(int arr[MAX][MAX], int row, int col) {
     printf("\nElements of matrix:\n");
     for (int i = 0; i < row; i++) {
