2ecb95cc404767cc49806cde4a6c220c38b400fb
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-03-05 00:05:24 +0530

    exit

--- queue.c
+++ queue.c
@@ -113,7 +113,7 @@ void peek()
         printf("\nToppest value of the QUEUE is :%d", QUEUE[FRONT]);
     }
 }
-// exit function
+
 void exitProgram()
 {
     printf("\nDo you really want to Exit program\nPRESS 1 to EXIT and 0 to CONTINUE\n");
