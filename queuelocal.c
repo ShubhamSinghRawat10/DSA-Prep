7b570fd64a743cd8845aaced35817c5b7e568a56
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-03-09 01:03:33 +0530

    dequeue

--- queuelocal.c
+++ queuelocal.c
@@ -33,7 +33,6 @@ void enqueue(int QUEUE[], int *FRONT, int *REAR)
         printf("Element %d is Enqueued Successfully!!!\n", enqueueElement);
     }
 }
-//dequeue function
 void dequeue(int QUEUE[], int *FRONT, int *REAR)
 {
     int dequeuedElement;
