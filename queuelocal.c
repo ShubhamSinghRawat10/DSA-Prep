58a03b1b6e59455b437df1dc2e3f7ee246fd1386
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-03-12 00:40:23 +0530

    isFull

--- queuelocal.c
+++ queuelocal.c
@@ -70,7 +70,6 @@ void traverse(int QUEUE[], int FRONT, int REAR)
         printf("\n");
     }
 }
-//isEmpty function and isFull function
 void isEmpty(int FRONT, int REAR)
 {
     if (FRONT == -1 && REAR == -1)
