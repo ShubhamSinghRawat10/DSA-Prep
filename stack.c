3302ed06944f8d59faf560c6e6094374f12c38e3
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-03-15 18:58:20 +0530

    Main

--- stack.c
+++ stack.c
@@ -51,7 +51,6 @@ void isempty(){
         printf("Enter the element if stack is not empty: %d\n",CAPACITY-top++);
     }
 }
-//pop 
 void pop(){
     if(top==-1){
         printf("stack underflow");
