bef195f7f4b1f7041578d912155fac63c276aebf
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-03-15 18:59:03 +0530

    Main

--- stack.c
+++ stack.c
@@ -32,7 +32,7 @@ void peek(){
         printf("Toppest element of stack is %d\n",stack[top]);
     }
 }
-//Is full
+//Is f
 void isfull(){
     if(top==CAPACITY-1){
         printf("Stack overflow");
