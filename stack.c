4595a5e86fd157d14a256a10434bca8cfc208f15
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-03-15 19:00:08 +0530

    Main

--- stack.c
+++ stack.c
@@ -18,8 +18,7 @@ void push(){
     else{
         // int value;
         printf("Enter the value you want to enter: ");
-        // scanf("%d",&value);
-    
+        // scanf
     scanf("%d",&stack[++top]);
     }
 }
