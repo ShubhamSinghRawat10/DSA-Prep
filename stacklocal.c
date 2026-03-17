b491b23d6da516b39246881c061cd587aa2cea73
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-03-17 22:24:13 +0530

    main

--- stacklocal.c
+++ stacklocal.c
@@ -20,7 +20,7 @@ void push(int stack[],int *top){
         printf("%d pushed to stack",value);
     }
 }
-//peek 
+//pe
 void peek(int stack[],int top){
     if(top==-1){
         printf("stack underflow");
