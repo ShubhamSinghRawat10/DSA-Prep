2358838ec4b6161755bf228ff741548a86a8523b
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-03-17 22:20:12 +0530

    main

--- stacklocal.c
+++ stacklocal.c
@@ -60,7 +60,7 @@ void pop(int stack[],int *top){
         top--;
     }
 }
-//dis
+//d
 void Display(int stack[], int top){
     printf("......Displaying stack..... ");
     printf("Enter elements in stack area \n");
