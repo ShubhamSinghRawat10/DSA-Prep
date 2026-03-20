89bf5fd38680fafb48f42b331ea765f833695a85
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-03-21 01:23:20 +0530

    code

--- struct.c
+++ struct.c
@@ -22,7 +22,7 @@ int main()
     printf("Enter marks: ");
     scanf("%f", &s2.marks);
 
-    //display ke liye h yeh code okk
+    //display ke liye h yeh code
     printf("\nStudent Details:\n");
     printf("Roll Number: %d\n", s1.rollno);
     printf("Name: %s\n", s1.name);
