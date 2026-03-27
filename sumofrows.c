7f3516573c41af9de9fd6778a923721f968f374f
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-03-28 02:49:47 +0530

    function

--- sumofrows.c
+++ sumofrows.c
@@ -24,7 +24,7 @@ void printArr(int arr[MAX][MAX], int row, int col) {
         printf("\n");
     }
 }
-//transpose function
+//transpose 
 void transpose1(int arr[MAX][MAX],int transpose[MAX][MAX],int row,int col)
 {
     for(int i=0;i<row;i++)
