41b3f5b1b839d6d54bb84f4fbcf278e9a5a1932c
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-04-06 00:40:37 +0530

    function

--- stacklocal.c
+++ stacklocal.c
@@ -65,7 +65,7 @@ void Display(int stack[], int top){
         printf("%d\n",stack[i]);
     }
 }
-/ergegwgsswgtrgdrghdrhgd
+/ergegwgsswgtrgdrghdrhg
 int main(){
     int stack[CAPACITY];
     int top=-1;
