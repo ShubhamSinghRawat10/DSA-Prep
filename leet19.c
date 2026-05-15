940fe1bf4782520013122522ddf149e9e059a2f3
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-05-16 01:26:14 +0530

    queue

--- leet19.c
+++ leet19.c
@@ -9,7 +9,7 @@ void insertelement()
     scanf("%d",&row);
     printf("Enter col pos: ");
     scanf("%d",&col);
-    //hRGWESGWEFF
+    //hRGWESGWEF
     printf("Enter value pos: ");
     scanf("%d",&value);
     for(int j=col-1 ; j>col-1; j--)
