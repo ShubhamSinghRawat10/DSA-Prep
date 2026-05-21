a92f01b52040516f89bd53091cd7c77559b5f0c0
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-05-22 00:45:33 +0530

    Queue

--- queuelocal.c
+++ queuelocal.c
@@ -144,7 +144,7 @@ int main()
         printf("7. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-
+/scascajnclanclkasnclkamc
         switch (choice)
         {
         case 1:
