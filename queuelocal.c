7e3e9d8536d39e6eaa7acb37e7e9e5409612bcd0
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-05-22 00:46:46 +0530

    Queue

--- queuelocal.c
+++ queuelocal.c
@@ -144,7 +144,7 @@ int main()
         printf("7. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-/scascajncl        switch (choice)
+/scascajnc        switch (choice)
         {
         case 1:
             enqueue(QUEUE, &FRONT, &REAR);
