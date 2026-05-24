43f722a904876016adcabd06c58ad5df505116a0
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-05-25 02:40:27 +0530

    codes

--- queuelocal.c
+++ queuelocal.c
@@ -146,7 +146,7 @@ int main()
         scanf("%d", &choice);
         switch (choice)
         {
-                 /;esfsefjsw   case 1:
+                 /;esfsefj  case 1:
             enqueue(QUEUE, &FRONT, &REAR);
             break;
         case 2:
