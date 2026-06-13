5131d5a87b7f85e393a9e14ddc3881530e3a7060
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2026-06-14 01:05:50 +0530

    isfull

--- circularqueue.c
+++ circularqueue.c
@@ -88,7 +88,7 @@ void isEmpty()
         printf("Queue will be Empty after Deqeueing %d Elements !!\n", (REAR + 1) - FRONT);
     }
 }
-// isFull fu
+// isFull f
 void isFull()
 {
     if (REAR == QUEUE_MAX - 1)
